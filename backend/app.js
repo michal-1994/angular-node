fae6d04444498297e18b3999b0da9cce1ed1c64c
Michal <michalgrzegorczyk04@gmail.com>
2023-06-05 18:47:41 +0200

    fetching data from database

--- backend/app.js
+++ backend/app.js
@@ -45,21 +45,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: 1,
-      title: "First server-side post",
-      content: "This is coming from the server",
-    },
-    {
-      id: 2,
-      title: "Second server-side post",
-      content: "This is coming from the server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched succesfully!",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched succesfully!",
+      posts: documents,
+    });
   });
 });
 
