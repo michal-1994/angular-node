10d1a914274f14b9e9d079a7fd3224522edcd20f
Michal <michalgrzegorczyk04@gmail.com>
2023-06-05 16:42:01 +0200

    connect to the mongodb and create env file

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://michal:${process.env.PASS}@cluster0.b8gq0tj.mongodb.net/`
+  )
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +34,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully",
