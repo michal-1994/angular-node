d96f15750e8c5d63a2f7ffa754fdfcb420dddc85
Michal <michalgrzegorczyk04@gmail.com>
2023-07-22 19:16:21 +0200

    addinng signup component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  isLoading: boolean = false;
+
+  onSignup(form: NgForm) {
+    console.log(form);
+  }
+}
