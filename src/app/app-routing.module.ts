4cd0d3d61c7144a4df488203226581fe0f661d85
Michal <michalgrzegorczyk04@gmail.com>
2023-07-22 19:06:34 +0200

    adding the login input fields

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 import { PostListComponent } from './posts/post-list/post-list.component';
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'edit/:postId',
     component: PostCreateComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
