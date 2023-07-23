a8e9af241e7464ea3eb3f3457ba641e06e70901e
Michal <michalgrzegorczyk04@gmail.com>
2023-07-23 18:56:59 +0200

    protecting resources with autorization

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -65,8 +65,15 @@ router.patch(
       content: req.body.content,
       imagePath: imagePath,
     });
-    Post.updateOne({ _id: req.params.id }, post).then((result) => {
-      res.status(200).json({ message: "Updated successful" });
+    Post.updateOne(
+      { _id: req.params.id, creator: req.userData.userId },
+      post
+    ).then((result) => {
+      if (result.modifiedCount) {
+        res.status(200).json({ message: "Updated successful" });
+      } else {
+        res.status(401).json({ message: "Not authorized" });
+      }
     });
   }
 );
@@ -104,9 +111,15 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({ message: "Post deleted!" });
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    (result) => {
+      if (result.deletedCount) {
+        res.status(200).json({ message: "Post deleted!" });
+      } else {
+        res.status(401).json({ message: "Not authorized" });
+      }
+    }
+  );
 });
 
 module.exports = router;
